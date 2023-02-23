d4e73017060d85e3f77ef6c00be00d56f5b5df14
AswinWebDev <aswinvinod0014@gmail.com>
2023-02-23 14:49:49 +0530

    added readme

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -14,27 +14,58 @@ function SearchBar({ onSubmit }) {
     setTerm(e.target.value);
   };
   return (
-    <div className="search-bar">
-      <form onSubmit={handleFormSubmit}>
-        <label>Enter Search Term</label>
-        <input
-          value={term}
-          onChange={handleChange}
-          type="text"
-          className="form-control"
-          placeholder="Search whatever you like!"
-        />
-        <button
-          className="btn btn-outline-secondary"
-          type="button"
-          id="button-addon2"
-          onClick={handleFormSubmit}
+    <nav className="navbar mb-2" style={{ backgroundColor: "#ffffcc" }}>
+      <div className="container-fluid">
+        <a className="navbar-brand">iMAGEN</a>
+        <form
+          className="d-flex justify-content-center"
+          role="search"
+          onSubmit={handleFormSubmit}
         >
-          Search
-        </button>
-      </form>
-    </div>
+          <input
+            className="form-control me-3"
+            onChange={handleChange}
+            value={term}
+            type="search"
+            placeholder="Search whatever!"
+            aria-label="Search"
+          />
+          <button
+            className="btn btn-outline-secondary"
+            type="submit"
+            id="button-addon2"
+            onClick={handleFormSubmit}
+          >
+            Search
+          </button>
+        </form>
+      </div>
+    </nav>
   );
 }
 
 export default SearchBar;
+
+//
+
+// //
+// <div classNameName="search-bar">
+//   <form onSubmit={handleFormSubmit}>
+//     <label>Enter Search Term</label>
+//     <input
+//       value={term}
+//       onChange={handleChange}
+//       type="text"
+//       classNameName="form-control"
+//       placeholder="Search whatever you like!"
+//     />
+//     <button
+//       classNameName="btn btn-outline-secondary"
+//       type="button"
+//       id="button-addon2"
+//       onClick={handleFormSubmit}
+//     >
+//       Search
+//     </button>
+//   </form>
+// </div>;
